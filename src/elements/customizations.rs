2c433a3a407dd87c7e256cebdd83c82eb7d01da2
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2026-01-12 14:48:30 +0100

    correct attributes

--- src/elements/customizations.rs
+++ src/elements/customizations.rs
@@ -3,16 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::{elements::is_element::IsElement, error::ValidationError};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Customizations {
-    #[serde(rename = "@id")]
-    pub id: String,
-    #[serde(rename = "@fullName")]
-    pub full_name: Option<String>,
-    #[serde(rename = "@version")]
-    pub version: Option<String>,
-    #[serde(rename = "@uri")]
-    pub uri: Option<String>,
-}
+pub struct Customizations(String);
 
 impl IsElement for Customizations {
     fn validate(&self, _strict: bool) -> Result<(), ValidationError> {
